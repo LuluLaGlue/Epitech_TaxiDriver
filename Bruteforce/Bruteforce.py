116f2a0a0393fab0fa936e042b6673024224f0c5
Lucien Sigayret <lucien.sigayret@protonmail.com>
2022-03-03 16:00:22 +0100

    updated bruteforce for optimisation

--- Bruteforce/Bruteforce.py
+++ Bruteforce/Bruteforce.py
@@ -8,48 +8,57 @@ def play(is_loop=False):
     total_steps = 0
     total_reward = 0
     done = False
+    passenger_found = False
 
     state = env.reset()
 
     while not done:
 
         while int(state / 100) != 0:
-            new_state, reward, done, _ = env.step(1)
+            new_state, reward, _, _ = env.step(1)
+
             if not is_loop:
                 env.render()
             state = new_state
             total_steps += 1
             total_reward += reward
+
         if not is_loop:
             print("TOP REACHED")
 
         while int(state / 10) != 0:
-            new_state, reward, done, _ = env.step(3)
+            new_state, reward, _, _ = env.step(3)
+
             if not is_loop:
                 env.render()
+
             total_steps += 1
             total_reward += reward
+
             if new_state == state:
+
                 if not is_loop:
                     print("HIT WALL")
-                new_state, reward, done, _ = env.step(0)
+
+                new_state, reward, _, _ = env.step(0)
                 total_reward += reward
                 total_steps += 1
-                new_state, reward, done, _ = env.step(0)
+                new_state, reward, _, _ = env.step(0)
                 total_reward += reward
                 total_steps += 1
-                new_state, reward, done, _ = env.step(3)
+                new_state, reward, _, _ = env.step(3)
                 total_reward += reward
                 total_steps += 1
-                new_state, reward, done, _ = env.step(3)
+                new_state, reward, _, _ = env.step(3)
                 total_reward += reward
                 total_steps += 1
-                new_state, reward, done, _ = env.step(1)
+                new_state, reward, _, _ = env.step(1)
                 total_reward += reward
                 total_steps += 1
-                new_state, reward, done, _ = env.step(1)
+                new_state, reward, _, _ = env.step(1)
                 total_reward += reward
                 total_steps += 1
+
                 if not is_loop:
                     env.render()
                 break
@@ -57,15 +66,24 @@ def play(is_loop=False):
         if not is_loop:
             print("TOP LEFT REACHED")
             print("Attempting pick up")
-        new_state, reward, done, _ = env.step(4)
-        total_reward += reward
-        total_steps += 1
-        state = new_state
-        passenger_found = False
-        if reward == -1:
-            passenger_found = True
-            if not is_loop:
-                print("Passenger Found...")
+        if not passenger_found:
+            new_state, reward, _, _ = env.step(4)
+            total_reward += reward
+            total_steps += 1
+            state = new_state
+            if reward == -1:
+                passenger_found = True
+                if not is_loop:
+                    print("Passenger Found...")
+        else:
+            new_state, reward, done, _ = env.step(5)
+            total_reward += reward
+            total_steps += 1
+
+            if done:
+                if not is_loop:
+                    print("Passenger Dropped Off")
+                break
         new_state, reward, done, _ = env.step(0)
         total_reward += reward
         total_steps += 1
@@ -216,19 +234,21 @@ if __name__ == "__main__":
 
     start = time.time()
     mean_steps, mean_result = 0, 0
+    max_steps = []
     is_loop = True if args.loop != 1 else False
 
     env = gym.make("Taxi-v3")
 
     for l in range(args.loop):
         steps, result = play(is_loop=is_loop)
+        max_steps.append(steps)
         mean_steps += steps
         mean_result += result
 
     if is_loop:
         print()
         print(
-            "[{} LOOP DONE -  {} SECONDES] - Mean Steps Per Loop: {} - Mean Reward Per Loop: {}"
+            "[{} LOOP DONE -  {} SECONDES] - Mean Steps Per Loop: {} - Max Steps For a Loop: {} - Mean Reward Per Loop: {}"
             .format(args.loop, np.round(time.time() - start, 4),
-                    np.round(mean_steps / args.loop, 2),
+                    np.round(mean_steps / args.loop, 2), np.max(max_steps),
                     np.round(mean_result / args.loop, 2)))
